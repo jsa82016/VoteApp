0c3fbf1c2ad325d2976637942b0e9ae7ced61c1d
SANAGUSTIN\Admin <jsa82016@gmail.com>
2020-10-14 14:13:29 +0800

    Initial check-in for VoteApp code

---
+++ VoteApp/src/test/java/com/jsa82016voteapp/VoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsa82016voteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
