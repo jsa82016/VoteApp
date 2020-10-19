90e94b2a66828d89a3dd75c3a0b78beb6201c3b6
SANAGUSTIN\Admin <jsa82016@gmail.com>
2020-10-19 13:52:15 +0800

    Enabling CSRF Protection

--- VoteApp/src/main/java/com/jsa82016voteapp/AppSecurityConfiguration.java
+++ VoteApp/src/main/java/com/jsa82016voteapp/AppSecurityConfiguration.java
@@ -29,7 +29,7 @@ public class AppSecurityConfiguration  extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http
 			.authorizeRequests()
 				.anyRequest().hasRole("USER").and()
 			.formLogin()
