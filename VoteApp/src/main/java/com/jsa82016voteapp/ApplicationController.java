acc142fdda21419191b64995b0357670c420e51c
SANAGUSTIN\Admin <jsa82016@gmail.com>
2020-10-19 13:15:31 +0800

    Creating Spring Security with custom Login page

---
+++ VoteApp/src/main/java/com/jsa82016voteapp/ApplicationController.java
@@ -0,0 +1,20 @@
+package com.jsa82016voteapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+	
+	@GetMapping("/")
+	public String rootView() {
+		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+		
+	}
+	
+}
